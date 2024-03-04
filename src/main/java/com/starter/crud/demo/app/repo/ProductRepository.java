00a82861b1a748639d12e8ea868819e5d148a4a2
GujarkarVaibhav <gujarkarvaibhav@gmail.com>
2024-03-04 13:53:35 +0530

    First Push

---
+++ src/main/java/com/starter/crud/demo/app/repo/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.starter.crud.demo.app.repo;
+
+import com.starter.crud.demo.app.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+}
